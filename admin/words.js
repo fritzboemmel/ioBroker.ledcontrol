ccb24f3ccf738e644d259bb297ac18e0e57ca2bc
Jan Hokamp <jan.hokamp@gmail.com>
2020-11-02 02:25:55 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'ledcontrol adapter settings': {
+		'en': 'Adapter settings for ledcontrol',
+		'de': 'Adaptereinstellungen für ledcontrol',
+		'ru': 'Настройки адаптера для ledcontrol',
+		'pt': 'Configurações do adaptador para ledcontrol',
+		'nl': 'Adapterinstellingen voor ledcontrol',
+		'fr': "Paramètres d'adaptateur pour ledcontrol",
+		'it': "Impostazioni dell'adattatore per ledcontrol",
+		'es': 'Ajustes del adaptador para ledcontrol',
+		'pl': 'Ustawienia adaptera dla ledcontrol',
+		'zh-cn': 'ledcontrol的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
